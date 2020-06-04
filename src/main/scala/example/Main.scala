2ae8ba380a0bc74be326a1c4a40059db2cd6bea6
Andrey P. Kovbovich <akovbovich@gmail.com>
2020-06-04 04:31:53 +0300

    cleanup

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -6,7 +6,7 @@ object Main extends App {
   val res =
     try {
       val (a, b) = (args(0).toInt, args(1).toInt)
-      (new MathSqrtOptSolution(2, 1_000_000_000)).solution(a, b)
+      MathSqrtOptSolution(2, 1_000_000_000).solution(a, b)
     } catch {
       case _: Throwable =>
         println("invalid args")
